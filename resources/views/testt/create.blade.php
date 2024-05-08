76d0129812479592f390e10912eed1d775760f04
Leonardo Liulle Reis Duarte <c28028@santacasaba.local>
2024-05-08 09:41:48 -0300

    Second

--- resources/views/testt/create.blade.php
+++ resources/views/testt/create.blade.php
@@ -6,8 +6,24 @@
     <title>Testt Compile</title>
 </head>
 <body>
-    
-    <h1>Insira a mensagem de envio</h1>
+
+
+<form method="post" action="">
+    @csrf
+    <div>
+        <label for="msg">Mensagem:</label>
+        <input type="text" id="msg" name="msg" required>
+    </div>
+    <div>
+        <label for="pass">senha:</label>
+        <input type="password" id="pass" name="pass" required>
+    </div>
+    <div>
+        <!-- <label for="ip">IP Address:</label> -->
+        <input type="text" id="ip" name="ip" value="{{substr($_SERVER['REMOTE_ADDR'],-2)}}" hidden>
+    </div>
+    <button type="submit">Submit</button>
+</form>
 
 </body>
 </html>
